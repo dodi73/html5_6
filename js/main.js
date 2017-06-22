9826ea733cdc28dbc3522e486ad1a0659d83e768
toth8j <toth.jozsef8@mav-szk.hu>
2017-06-22 14:48:22 +0200

    HTML5_6 feltöltése 8.nap 2. video Bootstrap

--- js/main.js
+++ js/main.js
@@ -26,5 +26,36 @@ function changeProgress( progress ) {
     
     var bar=document.querySelector(".progress .progress-bar");
     bar.style.width = progress + "%";
-    
 }
+
+//Késleltetett megjelenítés, ezredmásodpercben.
+setTimeout( function() {
+    $( "#myModal" ).modal( "show" );
+}, 5000 );
+
+//Ha nem anonomous függvénnyel adom meg (nem definíciót adok meg), hanme egyből beleírom
+//az utasítást a setTimeout fgv.-be, egyből le fog futni:
+/*
+setTimeout( $( "#myModal" ).modal( "show" ), 5000 );
+*/
+
+//Késleltetett megjelenítés, csak a fügvényt kívül definiálom, ugyanaz lesz mint az 
+//elsőnél. Ennek akkor van értelme, ha showModal() függvényt máshol is fel szeretném használni.
+//Ha a setTimeout( showModal() )-on belül "()"-el adom meg a függvényt, szintén egyből lefut.
+//Ez a különbség a definiálás és a futtatás között.
+/*
+function showModal() {
+    $( "#myModal" ).modal( "show" );    
+}
+setTimeout( showModal, 5000 );
+*/
+
+
+
+
+
+
+
+
+
+
